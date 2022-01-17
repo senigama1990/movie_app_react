87dc50e4ed72821c9c6a24fab91f28fc67580f04
senigama1990 <senigama1990@mail.ru>
2022-01-17 11:27:52 +0500

    films list

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import Movies from '../components/Movies'
 
-function Main() {
+class Main extends React.Component {
+
+  state = {
+    movies: []
+  }
+
+  componentDidMount() {
+    fetch('http://www.omdbapi.com/?apikey=329ffa13&s=panda')
+      .then(response => response.json())
+      .then(data => this.setState({ movies: data.Search}))
+  }
+
+  render() {
     return (
-        <div className='container content'>
-            <h1>All contents</h1>
-        </div>
+      <div className='container content'>
+        {this.state.movies.length ? (<Movies movies={this.state.movies} />) : <h1>Loading...</h1>}
+      </div>
     )
+  }
 }
 
 export default Main
